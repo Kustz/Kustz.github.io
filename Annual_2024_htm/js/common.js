a9a0c43da5bdc57e63e098ffd2a456c48367df68
Fan-Jiang Syuan <gramxs1123@gmail.com>
2025-10-13 05:22:06 +0800

    Add files via upload

---
+++ Annual_2024_htm/js/common.js
@@ -0,0 +1,62 @@
+AOS.init({
+	once: true
+});
+
+$(function(){
+
+	lightbox.option({
+		'albumLabel': "圖片 %1/%2",
+		'alwaysShowNavOnTouchDevices': true,
+		'disableScrolling': true
+	})
+
+	/*------------------*/
+
+	let scrollVal = $(this).scrollTop();
+
+	let checkScroll = ()=>{
+		if(scrollVal>($(window).height()/8)) {
+			$('.g_header').addClass('is_shrink');
+		}else{
+			$('.g_header').removeClass('is_shrink');
+		}
+		if(scrollVal>($(window).height())) {
+			$('.fixed_btn').addClass('is_show');
+		}else{
+			$('.fixed_btn').removeClass('is_show');
+		}
+	};
+
+	checkScroll();
+
+	let counterTri = true;
+
+	$(window).scroll(()=>{
+		scrollVal = $(this).scrollTop()
+		checkScroll();
+	})
+
+	var $scrollTopNow = 0,
+		$body = $("html, body");
+
+	function bScroll(tarTop, aniDuration) {
+		$body.animate({
+			scrollTop: tarTop
+		}, aniDuration);
+	}
+
+	$('.js_page_scroll').on('click', function(e) {
+		$scrollTopNow = $('html, body').scrollTop();
+		var $tar = $(this),
+			$tarTop = $($tar.attr('href')).offset().top-60,
+			$aniDuration = Math.abs($tarTop-$scrollTopNow)*.6 > 1000 ? 1000 : (Math.abs($tarTop-$scrollTopNow)*.6);
+		bScroll($tarTop, $aniDuration);
+		e.preventDefault();
+	});
+	
+	$('.js_scroll_top').on('click', function(e) {
+		bScroll(0, 100);
+		e.preventDefault();
+	});
+
+})
